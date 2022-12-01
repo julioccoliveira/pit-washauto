b9b97c4c1d8fae6c6c5f8b5ee0288dfd5b2a7de4
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2022-11-30 22:09:21 -0300

    Adicionando mocks dos endpoints iniciais
    Organizados por verbos e alfa

--- back/src/endpoint.ts
+++ back/src/endpoint.ts
@@ -2,4 +2,36 @@ import express from 'express'
 
 const api = express()
 
+//get
+api.get('/usuario/login', (req, res) => {
+    res.statusCode = 200
+    res.send('TODO LOGIN')
+    res.end
+})
+
+api.get('/servico/listar', (req, res) => {
+    res.statusCode = 200
+    res.send('TODO LISTAR')
+    res.end
+})
+
+//put
+api.put('/servico/finalizar/:id', (req, res) => {
+    res.statusCode = 200
+    res.send('TODO FINALIZAR SERVICO')
+    res.end
+})
+
+api.put('/servico/iniciar/add', (req, res) => {
+    res.statusCode = 200
+    res.send('TODO ADD PEDIDO')
+    res.end
+})
+
+api.put('/usuario/cadastrar/:id', (req, res) => {
+    res.statusCode = 200
+    res.send('TODO CADASTRAR USUARIO')
+    res.end
+})
+
 export default api
